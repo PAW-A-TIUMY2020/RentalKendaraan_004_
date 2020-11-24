67a505f80a705c5a40b127c4e7416b5820d6f02b
Nanda yudha pratama <nanda.yudha.ft18@mail.umy.ac.id>
2020-11-25 00:07:11 +0700

    Test

---
+++ RentalKendaraan_004/Controllers/PaginatedList.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RentalKendaraan_004.Controllers
+{
+    internal class PaginatedList<T>
+    {
+        internal static Task<string> CreateAsync<TEntity>(IQueryable<TEntity> queryable, int v, int pageSize) where TEntity : class
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
